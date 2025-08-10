9767e052019e2d4d838e508bd962c815f5d62dd4
Alejoho <alejoreilly96@gmail.com>
2025-08-09 20:05:15 -0400

    Load The WorkItem Page Content Using JavaScript

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 (function (app) {
 
+    app.portfolioProjects = [];
+    app.currentProject = {};
+
     app.homepage = function () {
-        setCurrentYear();
+        setCopyrightYear();
         wireContactForm();
     }
 
     app.portfolio = function () {
-        setCurrentYear();
+        setCopyrightYear();
     }
 
     app.workItem = function () {
-        setCurrentYear();
+        setCopyrightYear();
+        loadProjectsData();
+        loadCurrentProject();
+        updatePageData();
     }
 
-    function setCurrentYear() {
+    function setCopyrightYear() {
         const year = new Date().getFullYear();
         const spanYear = document.getElementById('copyright-year');
         spanYear.innerText = year;
@@ -40,4 +46,55 @@
         email.value = '';
         message.value = '';
     }
-})(window.app = window.app || {})
+
+    async function loadProjectsData() {
+        const cacheData = sessionStorage.getItem('projects');
+
+        if (cacheData !== null) {
+            app.portfolioProjects = JSON.parse(cacheData);
+        } else {
+            const rawData = await fetch('sitedata.json');
+            const data = await rawData.json();
+            app.portfolioProjects = data;
+            sessionStorage.setItem('projects', JSON.stringify(data));
+        }
+    }
+
+
+    function loadCurrentProject() {
+        const params = new URLSearchParams(window.location.search);
+        let item = Number.parseInt(params.get('item'));
+
+        if (item > app.portfolioProjects.length || item < 1) {
+            item = 1;
+        }
+
+        app.currentProject = app.portfolioProjects[item - 1];
+        app.currentProject.id = item;
+    }
+
+    function updatePageData() {
+        const h2 = document.getElementById('work-item-header');
+        h2.innerText = `0${app.currentProject.id}. ${app.currentProject.title}`;
+
+        const img = document.getElementById('work-item-img');
+        img.src = app.currentProject.largeImage;
+        img.alt = app.currentProject.largeImageAlt;
+
+        const projectP = document.querySelector('#project-text>p');
+        projectP.innerText = app.currentProject.projectText;
+
+        const challengesP = document.querySelector('#challenges-text>p');
+        challengesP.innerText = app.currentProject.challengesText;
+
+        const ul = document.querySelector('#technologies-text>ul');
+        const fragment = new DocumentFragment();
+        app.currentProject.technologies.forEach((t) => {
+            const li = document.createElement('li');
+            li.innerText = t;
+            fragment.appendChild(li);
+        });
+        ul.textContent = '';
+        ul.appendChild(fragment);
+    }
+})(window.app = window.app || {});
